e548038576ba72b6e7f0b24c0fc4a6e1b575363e
Cristian <crisfegomez@gmail.com>
2023-07-30 19:24:25 -0500

    primera parte clase 13

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
-
-const defaultTodos = [
-  {text: 'Cortar Cebolla', completed: true},
-  {text:'Tomar el Curso de Intro a Rect.js', completed:false},
-  {text: 'Llorar con la llorona', completed:false},
-  {text: 'LALALALALA', completed:false}
-];
-
+// LocalStorage, Comentamos los defaulttodos
+// const defaultTodos = [
+//   {text: 'Cortar Cebolla', completed: true},
+//   {text:'Tomar el Curso de Intro a Rect.js', completed:false},
+//   {text: 'Llorar con la llorona', completed:false},
+//   {text: 'LALALALALA', completed:false}
+// ];
+ 
+// Al haber comentado el array dentro de esta funcion ya no lo recibiriamos sino tendriamos el argumento desde localStorage. Pero no lo vamos a llamar directamente desde ahi.
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos); 
-  
+  // Vamos a crear una variable que sea igual a conseguir el Item, a TODOS_V1. Con esto lo que conseguimos es lograr que la aplicacion revise si hay algo en el localStorage y si este tiene informacion de nuestros TODOS entonces ese sera nuestro estado inicial. Y esta la pasamos a la funcion que renderiza nuestros Todos 
+  let parsedTodos = localStorage.getItem('TODOS_V1'); 
+
+  const [todos, setTodos] = React.useState(parsedTodos); 
   const [searchValue, setSearchValue]= React.useState ('');
 
   const completedTodos = todos.filter(
@@ -28,34 +31,24 @@ function App() {
       return todo.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
     }
   );  
-  // C O M P L E T A R  T O D O S  
-  // Una funcion que recibe como argumento un texto cuando se ejecuta el evento Onclick
+
   const completeTodo = (text) => {
-    // Una constante que llama al array existente todos y crea una copia denominada newTodos
     const newTodos = [...todos];
-    // Una variable que se define como el metodo de encontrar el mismo texto que recibimos como argumento dentro del array ahora denominado newTodos
     const todoIndex = newTodos.findIndex(
       (todo)=>todo.text == text
-      // si se encuentra dicho texto exactamanete igual a alguno que se encuentra en nuestro array este pasa a ser el text, o argumento que habiamos recibido.
     ) 
     newTodos[todoIndex].completed = 'true';
-    // ya teniendo el index del objeto o todo a cambiar, que el index como anteriormente definimos es el texto, esta linea cambia su propiedad completed a true.
 
     setTodos(newTodos);
-    //llamamos el actualizador del estado y enviamos el array modificado.
+
   };
-// E L I M I N A R  T O D O S
-   // Una funcion que recibe como argumento un texto cuando se ejecuta el evento Onclick
   const deleteTodo = (text) => {
-  // Una constante que llama al array existente todos y crea una copia denominada newTodos
   const newTodos = [...todos];
-  // Una variable que se define como el metodo de encontrar el mismo texto que recibimos como argumento dentro del array ahora denominado newTodos
   const todoIndex = newTodos.findIndex(
     (todo)=>todo.text == text
-    // si se encuentra dicho texto exactamanete igual a alguno que se encuentra en nuestro array este pasa a ser el text, o argumento que habiamos recibido.
   ) 
   newTodos.splice(todoIndex, 1);
-  // Con esta oracion lo que hacemos es eliminar el todo que requerimos iniciando desde el index del objeto del todo que escogimos y eliminando solamente un todo u objeto.
+
   setTodos(newTodos);
   }
   
