104ca7afbb04c524195de70163f7458b27790af1
Cristian <crisfegomez@gmail.com>
2023-08-01 11:46:51 -0500

    Parte 3 clase 13

--- src/App.js
+++ src/App.js
@@ -19,14 +19,31 @@ import React from 'react';
 // const stringifiedTodos = JSON.stringify(defaultTodos)
 // JSON.parse(stringifiedTodos)
 
+// localStorage.setItem ('TODOS_V1', stringifiedTodos)
+// const localStorageTodos = localStorage.getItem('TODOS_V1')
+
+// let parsedItems = JSON.parse(localStorageTodos)
+// parsedItems
+
 // Al haber comentado el array dentro de esta funcion ya no lo recibiriamos sino tendriamos el argumento desde localStorage. Pero no lo vamos a llamar directamente desde ahi.
 function App() {
   // Vamos a crear una variable que sea igual a conseguir el Item, a TODOS_V1. Con esto lo que conseguimos es lograr que la aplicacion revise si hay algo en el localStorage y si este tiene informacion de nuestros TODOS entonces ese sera nuestro estado inicial. Y esta la pasamos a la funcion que renderiza nuestros Todos 
   const localStorageTodos = localStorage.getItem('TODOS_V1');
   // lo que realizaremos para solucionar la renderizacion de nuestros objetos es crear una nueva variable que llame a nuestro codigo en el local Stroge, y en nuestro parsedTodos lo que realizaremos es llamar a la funcion JSON.parse cpn el argumento de los localStorageTodos/
+ 
+  // La funcion JSON.parse es una funcion que sirve para analizar o convertir una cadena JSON, en un objeto JavaScript.
+   let parsedTodos;
+    // Para que no se rompa la aplicacion cuando no tenga nada en el local strorage, vamos a crear un condicional
+    // Si no hay nada en el localStorage, entonces nuestras varibles parsedTodos sea igual a un array vacio.
+    // Tambien deseo que de una vez el localStorag me guarde un string vacio con el nombre TODOS_V1, y un array vacio que vamos a tranformar en codigo JSON.
 
-  let parsedTodos = JSON.parse(localStorageTodos)
-
+    if (!localStorageTodos){
+      localStorage.setItem('TODOS_V1', JSON.stringify([]));
+      parsedTodos([]);
+    }else{
+      // parseTodos seria lo que sea en el local Storage transformandolo de cadena JSON a codigo JavaScript
+      parsedTodos = JSON.parse(localStorageTodos);
+    }
   const [todos, setTodos] = React.useState(parsedTodos); 
   const [searchValue, setSearchValue]= React.useState ('');
 
