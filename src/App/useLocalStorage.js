c4a2ef7eb594dfc03d4595030da869d29acaab25
Cristian <crisfegomez@gmail.com>
2023-08-22 11:49:12 -0500

    clase 19

--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -2,19 +2,26 @@ import React from "react";
 
 function useLocalStorage(itemName, initialValue){
 
-    const localStorageItem = localStorage.getItem(itemName);
+    const [item, setItem] = React.useState(initialValue);
+    // valor por defecto true para cuando este cargue sepamos que esta cargando nuestro initialValue
+    const [loading, setLoading] = React.useState(true);
+    const [error, setError] = React.useState(false);
    
-    let parsedItem;
-     
-    if (!localStorageItem){
-       localStorage.setItem(itemName, JSON.stringify(initialValue));
+     React.useEffect(()=> {
+      const localStorageItem = localStorage.getItem(itemName);  
+      if (!localStorageItem){
+        localStorage.setItem(itemName, JSON.stringify(initialValue));
+  
+        parsedItem= initialValue;
+      }else{
+        parsedItem = JSON.parse(localStorageItem);
+      }
+    
+    })
+    
+
  
-       parsedItem= initialValue;
-     }else{
-       parsedItem = JSON.parse(localStorageItem);
-     }
- 
-     const [item, setItem] = React.useState(parsedItem);
+   
      const saveItem = (newItem) => {
        localStorage.setItem(itemName, JSON.stringify(newItem));
        
